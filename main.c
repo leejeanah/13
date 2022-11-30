51a1a482b8eb75c2ba7a72d360009aa4e476be81
leejeanah <ijina590@naver.com>
2022-11-30 20:24:41 +0900

    02

--- main.c
+++ main.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_NAME      20
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct student{
-	int     ID;
-	char    name[MAX_NAME];
-	double  grade;
+struct point{
+	int      x;
+	int      y;
 };
 
-int main(int argc, char *argv[]) {
+int main(void){
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double dist;
 	
-	struct student student1 = {123456, "Juyeop", 4.2};
+	printf("input p1 coordinate(x y): ");
+	scanf("%d %d", &p1.x, &p1.y);
 	
-	student1.ID = 1234567;
-	strcpy(student1.name, "Jina");
-	student1.grade = 3.4;
+	printf("input p2 coordinate(x y): ");
+	scanf("%d %d", &p2.x, &p2.y);
 	
-	printf("ID: %i\n", student1.ID);
-	printf("name: %s\n", student1.name);
-	printf("grade: %f\n", student1.grade);
-
-	return 0;
+	//p1,p2간의 거리를 계산하고 출력
+	//math.h의 sqrt() 함수 활용 
+	
+	xdiff = p2.x - p1.x;
+	
+	ydiff = p2.y - p1.y;
+	
+	//dist = sqrt(xdiff^2 + ydiff^2)
+	dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+	
+	printf("distance: %f\n", dist);
+	
 }
